6540bb64f946b5a30d50e630700ee075d8c37d3f
Robin <robin.vdn@educationwarehouse.nl>
2023-11-08 16:35:31 +0100

    debug: more prints for set schema

--- src/edwh_migrate/migrate.py
+++ src/edwh_migrate/migrate.py
@@ -269,9 +269,11 @@ def recover_database_from_backup():
     if is_postgres:
         schema = "public"
         echo = plumbum.local["echo"]
-        cmd = echo[f"'SET search_path TO {schema};'"] | sql_consumer
-        print("For postgres: set schema to public", cmd)
-        cmd()
+        cmd = echo[f"SET search_path TO {schema};"] | sql_consumer
+        print("For postgres: set schema to public:")
+        print(cmd)
+        print(cmd())
+        print('end set schema')
 
 
 def activate_migrations():
